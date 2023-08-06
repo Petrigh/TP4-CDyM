d3e03cc1912b7fbf839be81c94d42aea3a4ca0e0
Petrigh <lylabono14@gmail.com>
2023-08-06 18:24:56 -0300

    fix bug pwm software

--- TP4-CDyM/TP4-CDyM/PWM.c
+++ TP4-CDyM/TP4-CDyM/PWM.c
@@ -1,8 +1,8 @@
 #include "PWM.h"
 
-#define REDSHADE 0x01
-#define GREENSHADE 0x00
-#define BLUESHADE 0x00
+#define REDSHADE 0x0001
+#define GREENSHADE 0x01
+#define BLUESHADE 0x01
 #define PWM_PERDIOD 250
 #define PWM_ON PORTB &=~(1<<PORTB5)
 #define PWM_OFF PORTB |=(1<<PORTB5)
@@ -13,7 +13,7 @@ typedef enum {UP, HOLD, DOWN, OFF} state;
 uint8_t fader = 0;
 uint8_t count = 0;
 state stateFlag;
-volatile uint16_t PWM_DELTA = 0x00FF;
+volatile uint16_t PWM_DELTA = 255;
 volatile uint8_t ocrValue = 0;
 volatile uint8_t adjustLED = 0;
 void PWM_soft_Update(void);
@@ -21,8 +21,8 @@ void PWM_soft_Update(void);
 void setupPWM() {
 	// Configuración para Timer1 canal A (LED azul) y canal B (LED verde)
 	DDRB |= (1 << PORTB1) | (1 << PORTB2); //Canal OCR0A y OCR0B
-	OCR1A = 0xFF; //Arranca apagado
-	OCR1B = 0xFF; //Arranca apagado
+	OCR1A = 0xFF;
+	OCR1B = 0xFF;
 	TCCR1A |= (1 << WGM10) | (1 << WGM10) | (1 << COM1A1) | (1 << COM1B1); // Modo PWM no-invertido de 8 bits en OC1A y OC1B
 	TCCR1B |= (1 << WGM12) | (1 << CS12); // preescalador de 256; frecuencia de 122Hz
 	//configuracion Mef
@@ -80,7 +80,7 @@ void setRGBColor() {
 		
 		case OFF:
 		
-				//TCCR0B &= ~(1<<CS02); //apago Timer0
+				TCCR0B &= ~(1<<CS01); //apago Timer0
 				TCCR1B &= ~(1<<CS12); //apago Timer1
 				PORTB |= (1<<PORTB1) | (1<<PORTB2) | (1<<PORTB5); //Enciendo LEDs
 			
@@ -88,9 +88,9 @@ void setRGBColor() {
 				if(++count == haltTop){
 					stateFlag = UP;
 					count = 0;
-					//TCCR0B |= (1<<CS02); //Enciendo Timer0
+					TCCR0B |= (1<<CS01); //Enciendo Timer0
 					TCCR1B |= (1<<CS12); //Enciendo Timer1
-					PORTB &= ~(1<<PORTB1) & ~(1<<PORTB2) & ~(1<<PORTB5); //Apago LEDs
+					PORTB &= ~((1<<PORTB1) | (1<<PORTB2) | (1<<PORTB5)); //Apago LEDs
 				}
 			}
 		break;
@@ -98,12 +98,11 @@ void setRGBColor() {
 }
 
 void setupTimer0CTC() {
-	// Configurar Timer1 en modo CTC con prescaler 256
-	TCCR0A = 0;  // Modo normal de operación
-	TCCR0B = (1 << CS02); // Prescaler 256
-	TCNT0 = 0;   // Inicializar el contador en 0
-	OCR0A = 63;  // Interrupción cada 4 ms (250Hz)
-	TIMSK0 = (1 << OCIE0A); // Habilitar la interrupción de comparación de Timer1 Canal A (PWM software)
+	// Configurar Timer0 en modo CTC con prescaler 256
+	TCCR0A |= (1 << WGM01);  // Modo CTC de operación
+	TCCR0B = (1 << CS01); // Prescaler 8
+	OCR0A = 39;  // Interrupción cada 4 ms (250Hz)
+	TIMSK0 = (1 << OCIE0A); // Habilitar la interrupción de comparación de Timer0 Canal A (PWM software)
 }
 
 
@@ -116,22 +115,22 @@ ISR(TIMER0_COMPA_vect) {
 
 //PWM por Software
 void PWM_soft_Update(void){
-	static uint16_t count = 0;
-	if (count < PWM_DELTA) {
+	static uint16_t pwm_position = 0;
+	if (pwm_position < PWM_DELTA) {
 		PWM_ON; // Establecer el pin en estado alto
 	} else {
 		PWM_OFF; // Establecer el pin en estado bajo
 	}
-	count = (count + 1) % 256; // Contador circular de 0 a 255
+	pwm_position = (pwm_position + 1) % 256; // Contador circular de 0 a 255
 }
 
 
 
 void setupTimer2CTC() {
 	// Configurar Timer1 en modo CTC con prescaler 256
-	TCCR2A |= (1 << WGM01); // Seteo Modo CTC
-	TCCR2B |= (1 << CS02); // Prescaler 256
-	OCR2A = 240; // Interrupción cada 4 ms (250Hz)
+	TCCR2A |= (1 << WGM21); // Seteo Modo CTC
+	TCCR2B |= (1 << CS22); // Prescaler 256
+	OCR2A = 249; // Interrupción cada 4 ms (250Hz)
 	TIMSK2 |= (1 << OCIE2A) ; // Habilitar la interrupción de comparación de Timer2 Canal A (Interrupcion periodica)
 }
 
